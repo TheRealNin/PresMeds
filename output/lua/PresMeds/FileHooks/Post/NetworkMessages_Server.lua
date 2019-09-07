c1cdc6a6c0879d1be4c04e529a46e4ec7970b479
Nin <therealnin@users.noreply.github.com>
2019-09-06 20:47:02 -0400

    Limit max number of droppacks

--- output/lua/PresMeds/FileHooks/Post/NetworkMessages_Server.lua
+++ output/lua/PresMeds/FileHooks/Post/NetworkMessages_Server.lua
@@ -1,4 +1,18 @@
 
+
+local kMaxPacks = 256
+
+local function SortPacksByAge(entities)
+	local function compareAge(a, b)
+		local age1 = a.expireTime
+		local age2 = b.expireTime
+
+		return age1 > age2
+	end
+	table.sort(entities, compareAge)
+end
+	
+
 local function OnRequestTech(client, message)
 
     local techId = message.techId
@@ -13,7 +27,26 @@ local function OnRequestTech(client, message)
 			local mapName = LookupTechData(techId, kTechDataMapName)
 			local position = player:GetOrigin() + Vector(0, 0.05, 0)
 			if mapName then
+			
+				local pack_list = GetEntitiesForTeam("DropPack", player:GetTeamNumber())
+				local total = #pack_list
+				if total >= kMaxPacks then
+				
+					local packs = {}
+					for _, pack in ipairs(pack_list) do 
+						table.insert(packs, pack)
+					end
+					
+					SortPacksByAge(packs)
+					while total >= kMaxPacks do
+						total = total - 1
+						local pack = table.remove(packs)
+						DestroyEntity(pack)
+					end
+				end
+				
 				CreateEntity(mapName, position, player:GetTeamNumber())
+				
 			end
 			
 		end
